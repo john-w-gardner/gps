37f50d50924d2a9be15cdca3d7e24e4149612b94
John Gardner <jgard76@yahoo.com>
2021-01-08 16:03:06 -0700

    moved getSatelliteArray() to take pointer to struct satInstance

--- receiver.c
+++ receiver.c
@@ -8,32 +8,16 @@ compute vehicle location and time using satellite data
 
 #include <stdio.h>
 #include <math.h>
+#include "receiver.h"
 
 #define LINELEN 200
 #define NSAT 24/* length of array holding visible satellites
                    impossible for all 24 to be visible, but.. */
 
-
-
-struct satInstance {
-  double x;
-  double y;
-  double z;
-  double t;
-  double idx;
-};
-
-struct vehInstance {
-  double x;
-  double y;
-  double z;
-  double t;
-};
-
 int main()
 {
   // get constants
-  readDataDat();
+  //readDataDat();
 
   // set up line buffer
   char line[LINELEN];
@@ -44,40 +28,25 @@ int main()
   struct satInstance satArray[NSAT]; 
 
   // while new epoch of satellites available
-  while (getSatelliteArray(&satArray, &nsat))
+  while (getSatelliteArray(satArray, &nsat))
     {
       epoch++; 
+      printSatellite(satArray[5]);
+      /*
       // calculations
       computeVehLocation(satArray, veh);
-      computet_v(satArray[0], veh); /* test here: compare results using several different satellites */
+      computet_v(satArray[0], veh); // test here: compare results using several different satellites 
       // convert coords and print
       unrotate(veh);
       printveh(veh);
      
       updateInitialGuess(); // for the next iteration/epoch
+      */
     } 
 }
 
-
-/* 
-ungetSat: push satellite back onto input (buffer) 
- */
-int ungetSat(struct satInstance *satBuf, struct satInstance *s)
+void printSatellite(struct satInstance s)
 {
-
-}
-
-
-
-
-
-
-// jam contents of s2 into s1
-void updateSat(struct satInstance *s1, struct satInstance *s2)
-{
-  s1->idx = s2->idx;
-  s1->x = s2->x;
-  s1->y = s2->y;
-  s1->z = s2->z;
-  s1->t = s2->t;
+  printf("satellite %d: \n", s.idx);
+  printf("x %.15lf y %.15lf z %.15lf t %.15lf\n", s.x, s.y, s.z, s.t);
 }
